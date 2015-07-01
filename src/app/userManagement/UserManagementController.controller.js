ccb14ce6275a51411b164176c324c58a7fdeb0cf
Tilman Potthof <tilmanpotthof@googlemail.com>
2015-07-01 16:30:00 +0200

    basic user management

--- src/app/userManagement/UserManagementController.controller.js
+++ src/app/userManagement/UserManagementController.controller.js
@@ -1,7 +1,29 @@
 angular.module('userManagement').controller('UserManagementController', function ($scope) {
   'use strict';
 
-  $scope.addUser = function () {
+  function arrayRemove(array, element) {
+    var index = array.indexOf(element);
+    array.splice(index, 1);
+  }
 
+  function initUser() {
+    $scope.user = {};
+  }
+
+  initUser();
+
+  var predefinedUser = {
+    vorname: 'Tilman',
+    nachname: 'Potthof'
+  };
+
+  $scope.users = [predefinedUser];
+
+  $scope.addUser = function (user) {
+    $scope.users.push(user);
+    initUser();
+  };
+  $scope.removeUser = function (user) {
+    arrayRemove($scope.users, user);
   };
 });
