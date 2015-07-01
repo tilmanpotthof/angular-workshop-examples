2eaef1c3cd5e726b8dcc8312560d81cc91525590
Tilman Potthof <tilmanpotthof@googlemail.com>
2015-07-01 23:17:29 +0200

    meals as select with ngOptions

--- src/app/userManagement/UserManagementController.controller.js
+++ src/app/userManagement/UserManagementController.controller.js
@@ -19,6 +19,20 @@ angular.module('userManagement').controller('UserManagementController', function
 
   $scope.users = [predefinedUser];
 
+  $scope.meals = [{
+    id: 1,
+    name: 'Pizza',
+    calories: 258
+  },{
+    id: 2,
+    name: 'Pommes',
+    calories: 299
+  },{
+    id: 3,
+    name: 'Kartoffeln',
+    calories: 89
+  }];
+
   $scope.addUser = function (user) {
     $scope.users.push(user);
     initUser();
